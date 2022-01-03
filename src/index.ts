3c99ad12678d250fa8f19bd30ecd4c2990b81fa5
Matt <matt@wbcs.ventures>
2022-01-02 23:32:29 -0800

    Add setField return value from hook

--- src/index.ts
+++ src/index.ts
@@ -17,7 +17,11 @@ export const useFormFields: (initialState: Params) => {
       [event.target.id]: event.target.value,
     });
   };
-  return { fields, handleFieldChange };
+  const setField = (fieldName: string, value: any) => setValues({
+    ...fields,
+    [fieldName]: value
+  })
+  return { fields, handleFieldChange, setField };
 };
 
 export const useMailChimpForm: (url: string) => {
